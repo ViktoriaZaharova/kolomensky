02efebe31e293d699c93f2a640f7fbaf46d4ea7e
viktoriazaharova <zaharova181114@gmail.com>
2020-11-27 09:07:21 +0200

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,3 @@
-$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
-    $(this)
-        .addClass('active').siblings().removeClass('active')
-        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-
-    $(".tabs__content.active .slick-slider").slick('reinit');
-});
-
 $('.tenants-slider').slick({
     slidesToShow: 4,
     prevArrow: '<button type="button" class="slick-prev">' +
@@ -16,6 +8,27 @@ $('.tenants-slider').slick({
         '<svg width="17" height="36" viewBox="0 0 17 36" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
         '    <path d="M1.5 1L15.5 18L1.5 35" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
         '</svg>\n</button>',
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+
+    ]
 });
 
 $('.shops-slider').slick({
@@ -28,10 +41,47 @@ $('.shops-slider').slick({
         '<svg width="17" height="36" viewBox="0 0 17 36" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
         '    <path d="M1.5 1L15.5 18L1.5 35" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
         '</svg>\n</button>',
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 890,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+
+    ]
+});
+
+$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+    $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+
+    $(".shops .tabs__content.active .slick-slider").slick('setPosition');
 });
 
 $("body").on("click", ".btn-scroll-top", function () {
     $("html, body").animate({
         scrollTop: 0
     }, "slow")
+});
+
+$('.btn-burger').click(function () {
+    $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').click(function () {
+    $('.mobile-menu').fadeOut();
 });
