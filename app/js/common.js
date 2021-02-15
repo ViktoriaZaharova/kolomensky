6a8ac8818adb47adbadfd7fd794b766683ee3030
Viktoria <zaharova181114@gmail.com>
2021-02-15 21:56:08 +0200

    add about

--- app/js/common.js
+++ app/js/common.js
@@ -64,6 +64,38 @@ $('.shops-slider').slick({
     ]
 });
 
+$('.about-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev">' +
+        '<svg width="17" height="36" viewBox="0 0 17 36" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '    <path d="M15.5 1L1.5 18L15.5 35" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+        '</svg>\n</button>',
+    nextArrow: '<button type="button" class="slick-next">' +
+        '<svg width="17" height="36" viewBox="0 0 17 36" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '    <path d="M1.5 1L15.5 18L1.5 35" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+        '</svg>\n</button>',
+    asNavFor: '.about-slider-preview'
+});
+
+$('.about-slider-preview').slick({
+    slidesToShow: 1,
+    focusOnSelect: true,
+    arrows: false,
+    asNavFor: '.about-slider',
+    variableWidth: true,
+    // infinite: false
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                infinite: true
+            }
+        }
+
+    ]
+});
+
 $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
     $(this)
         .addClass('active').siblings().removeClass('active')
